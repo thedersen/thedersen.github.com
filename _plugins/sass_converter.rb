7f28ebaf81a913a2e3414d2724588d1187ace905
thedersen <thedersen@gmail.com>
2012-07-07 03:48:19 +0200

    looka like I have to do without custome filters..

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -1,25 +1,25 @@
-module Jekyll
-  require 'sass'
-  class SassConverter < Converter
-    safe true
-    priority :low
+# module Jekyll
+#   require 'sass'
+#   class SassConverter < Converter
+#     safe true
+#     priority :low
 
-    def matches(ext)
-      ext =~ /scss/i
-    end
+#     def matches(ext)
+#       ext =~ /scss/i
+#     end
 
-    def output_ext(ext)
-      ".css"
-    end
+#     def output_ext(ext)
+#       ".css"
+#     end
 
-    def convert(content)
-      begin
-        puts "Performing Sass Conversion..."
-        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./stylesheets/"])
-        engine.render
-      rescue StandardError => e
-        puts "!!! SASS Error: " + e.message
-      end
-    end
-  end
-end
+#     def convert(content)
+#       begin
+#         puts "Performing Sass Conversion..."
+#         engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./stylesheets/"])
+#         engine.render
+#       rescue StandardError => e
+#         puts "!!! SASS Error: " + e.message
+#       end
+#     end
+#   end
+# end
