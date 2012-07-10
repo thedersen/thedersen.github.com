3a3b17fbd3770e0aae94040cc68ab635dd68ddd5
thedersen <thedersen@gmail.com>
2012-07-10 19:36:25 +0200

    Cleaned up the tracking code

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,6 +7,7 @@ jQuery(function($){
     updateHash: true,
     controls: 'postPagination'
   });
+
   $('#talks').easyPaginate({
     step: 5,
     numeric: false,
@@ -15,13 +16,23 @@ jQuery(function($){
     updateHash: true,
     controls: 'talkPagination'
   });
+
   $('#paginate').easyPaginate({
     step: 1,
     updateHash: false
   });
 
-  // $(window).bind('hashchange', function() {
-  //   var hash = window.location.hash.split('/');
-  //   $(hash[0]).easyPaginate('show', hash[1]);
-  // });
+  $('a').on('click',function(e){
+    var url = $(this).attr('href'), newTab = false;
+    if (e.currentTarget.host != window.location.host) {
+      _gat._getTrackerByName()._trackEvent('Outbound Links', e.currentTarget.host, url, 1);
+      if (e.metaKey || e.ctrlKey) {
+        newTab = true;
+      }
+      if (!newTab) {
+        e.preventDefault();
+        setTimeout('document.location = "' + url + '"', 100);
+      }
+    }
+  });
 });
